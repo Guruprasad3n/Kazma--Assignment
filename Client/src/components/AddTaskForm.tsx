e14a309c2639f86d2a5a681d050a8f469ec4d130
Guruprasad3n <gp76652@gmail.com>
2024-09-27 08:49:29 +0530

    Updated

--- Client/src/components/AddTaskForm.tsx
+++ Client/src/components/AddTaskForm.tsx
@@ -54,6 +54,7 @@ const AddTaskForm: React.FC<AddTaskFormProps> = ({ onAddTask }) => {
         value={task}
         onChange={(e) => setTask(e.target.value)}
         placeholder="Add a new task"
+        style={{ color: "black" }}
         className="flex-grow p-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
       />
       <button className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-200">
